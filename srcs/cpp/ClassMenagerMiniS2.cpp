7a8a4575bac6706ac3a147faa296ae3d00d57497
jujulie-exe <julie.exe.franco@gmail.com>
2026-01-08 22:29:05 +0100

    ADD: sequanze

--- srcs/cpp/ClassMenagerMiniS2.cpp
+++ srcs/cpp/ClassMenagerMiniS2.cpp
@@ -1,8 +1,9 @@
 #include "ClassMenagerMiniS2.hpp"
 /*♡♡♡♡♡♡♡♡♡♡♡CTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-ClassMenagerMiniS2::ClassMenagerMiniS2(int Handle)
-: _lgpio(Handle)
+ClassMenagerMiniS2::ClassMenagerMiniS2(int Handle, const std::vector<int> &pin)
+: _lgpio(Handle), _pin(pin), _claimPin(false), _maskBit(0)
 {
+    this->_pinVector = vector<int>(_pin.begin(), _pin.end());
     	
      int h = lgGpiochipOpen(_lgpio);
      if (h < 0)
@@ -17,4 +18,87 @@ ClassMenagerMiniS2::~ClassMenagerMiniS2()
     {
         throw noClose();
     }
+}
+/*♡♡♡♡♡♡♡♡♡♡♡COPY♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+ClassMenagerMiniS2::ClassMenagerMiniS2(const ClassMenagerMiniS2 &src)
+: _lgpio(src._lgpio), _pin(src._pin), _claimPin(false), _maskBit(0)
+{
+    this->_pinVector = vector<int>(_pin.begin(), _pin.end());
+    // TODO capire la gestone del claim per i copy e operatore =
+}
+
+bool ClassMenagerMiniS2::intClaimPin()
+{
+    if( lgGpioGroupClaimOutput(
+        _lgpio,
+        0,
+        _pinVector.size(),
+        _pinVector.data(),
+        0
+    ) < 0)
+    {
+        return false;
+    }
+    _claimPin = true;
+    _maskBitPin();
+    return true;
+}
+
+void ClassMenagerMiniS2::_maskBitPin()
+{
+    if (_claimPin)
+    {
+        for (size_t i = 0; i < _pinVector.size(); i++)
+        {
+            _maskBit |= (1ULL << i);
+        }
+    }
+}
+
+bool ClassMenagerMiniS2::_allPinOn()
+{
+    if (lgGroupWrite(_lgpio, _pinVector[0], _maskBit, _maskBit)  < 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool ClassMenagerMiniS2::_allPinOff()
+{
+    if (lgGroupWrite(_lgpio, _pinVector[0], 0, _maskBit)  < 0)
+    {
+        return false;
+    }
+    return true;
+}
+void ClassMenagerMiniS2::sequenceChase()
+{
+    if (!_claimPin) return;
+
+    for (size_t i = 0; i < _pinVector.size(); i++)
+    {
+        uint64_t mask = 1ULL << i;
+
+        // Accendi il pin i
+        lgGroupWrite(_lgpio, _pinVector[0], mask, mask);
+        usleep(200000); // 200ms
+
+        // Spegni il pin i
+        lgGroupWrite(_lgpio, _pinVector[0], 0, mask);
+        usleep(100000); // 100ms
+    }
+}
+/*♡♡♡♡♡♡♡♡♡♡♡OPERATOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+ClassMenagerMiniS2 & ClassMenagerMiniS2::operator=(const ClassMenagerMiniS2 &src)
+{
+    if (this != &src)
+    {
+        _lgpio = src._lgpio;
+        _pin = src._pin;
+        // TODO capire la gestone del claim per i copy e operatore = magari bisogna rilasciarli 
+        _claimPin = false;
+        this->_pinVector = vector<int>(_pin.begin(), _pin.end());
+    }
+    return *this;
 }
--- srcs/include/ClassMenagerMiniS2.hpp
+++ srcs/include/ClassMenagerMiniS2.hpp
@@ -1,16 +1,43 @@
 #include <lgpio.h> 
 #include <stdexcept>
 #include <iostream>
+#include <set>
+#include <vector>
 class ClassMenagerMiniS2
 {
+        /*♡♡♡♡♡♡♡♡♡♡♡PRIVATE♡♡♡♡♡♡♡♡♡♡♡♡♡*/
     private:
-        int _lgpio;
+        const int _lgpio;
+        const std::set<int> _pin;
+        const std::vector<int> _pinVector;
+        uint64_t _maskBit;
+        bool _claimPin;
+
+
+        
+        /*♡♡♡♡♡♡♡♡♡♡♡HEANDELPIN♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+        bool _allPinOn();
+        bool _allPinOff();
+        bool _onePinOn(int pin);
+        bool _onePinOff(int pin);
+        void _maskBitPin();
+
+
+
+
+        /*♡♡♡♡♡♡♡♡♡♡♡PUBLIC♡♡♡♡♡♡♡♡♡♡♡♡♡*/
     public:
          /*♡♡♡♡♡♡♡♡♡♡♡CTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
-        ClassMenagerMiniS2(int Handle);
-        ClassMenagerMiniS2(const ClassMenagerMiniS2 &src);
+        ClassMenagerMiniS2(int Handle, std::vector<int> pin);
          /*♡♡♡♡♡♡♡♡♡♡♡COPY♡♡♡♡♡♡♡♡♡♡♡♡♡*/
+        ClassMenagerMiniS2(const ClassMenagerMiniS2 &src);
+         /*♡♡♡♡♡♡♡♡♡♡♡OPERATOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
         ClassMenagerMiniS2 & operator=(const ClassMenagerMiniS2 &src);
+
+        bool intClaimPin();
+        bool sequenzaPin();
+
+
          /*♡♡♡♡♡♡♡♡♡♡♡DTOR♡♡♡♡♡♡♡♡♡♡♡♡♡*/
         virtual ~ClassMenagerMiniS2();
     class noOpen : public std::exception
